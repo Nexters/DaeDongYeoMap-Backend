4592497d6eda63636f54c56335e249cf5e301c6a
minkj1992 <minkj1992@gmail.com>
2021-01-09 18:20:43 +0900

    Add: 도메인 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmojiModule } from './emoji/emoji.module';
+import { CommentModule } from './comment/comment.module';
+import { SpotModule } from './spot/spot.module';
+import { PlaceModule } from './place/place.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [EmojiModule, CommentModule, SpotModule, PlaceModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
