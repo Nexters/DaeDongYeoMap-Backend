8c9494d88b112afa8a0504b1f18e5ab320a016e8
minkj1992 <minkj1992@gmail.com>
2021-01-10 00:25:10 +0900

    Add: 네이버 지역 검색 rest api 틀 생성
    - module: api 세팅
    - service: provider
    - controller: 검색 기능 controller

---
+++ src/place/place.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('place')
+export class PlaceController {}
--- src/place/place.module.ts
+++ src/place/place.module.ts
@@ -1,4 +1,18 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from "@nestjs/common";
+import { PlaceService } from "./place.service";
+import { PlaceController } from './place.controller';
 
-@Module({})
+@Module({
+  imports: [
+    HttpModule.register({
+      baseURL: `${process.env.NAVER_SEARCH_HOST}/${process.env.NAVER_SEARCH_FORMAT}`,
+      params: {
+        NAVER_SEARCH_CLIENT_ID: process.env.NAVER_SEARCH_CLIENT_ID,
+        NAVER_SEARCH_CLIENT_SECRET: process.env.NAVER_SEARCH_CLIENT_SECRET,
+      },
+    }),
+  ],
+  providers: [PlaceService],
+  controllers: [PlaceController],
+})
 export class PlaceModule {}
