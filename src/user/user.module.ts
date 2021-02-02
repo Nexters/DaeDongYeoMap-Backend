8b0d34213526015d23090725bf50e6116c323368
devgunho <devgun@kakao.com>
2021-02-02 20:58:23 +0900

    Add: user module ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
 
 @Module({})
-export class UserModule {}
+export class UserModule {
+    @Field()
+    userId: string;
+
+    @Field()
+    name: string;
+
+    @Field()
+    nickname: string;
+
+    @Field(() => Int)
+    email: number;
+
+    @Field()
+    partner_id: string;
+
+    @Field({ nullable: true })
+    isAuth?: boolean;
+}
