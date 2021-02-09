26bc5049a2ae71d945eccc1e2fc9ab9916dcff36
devgunho <devgun@kakao.com>
2021-02-10 03:23:43 +0900

    Implement USER to preform specific tasks

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,34 @@
+import {
+    BaseEntity,
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    Unique,
+    OneToMany,
+} from "typeorm";
+import * as bcrypt from "bcryptjs";
+
+
+@Entity()
+@Unique(["username"])
+export class User extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    salt: string;
+
+    @OneToMany((type) => Task, (task) => task.user, { eager: true })
+    tasks: Task[];
+
+    async validatePassword(password: string): Promise<boolean> {
+        const hash = await bcrypt.hash(password, this.salt);
+        return hash === this.password;
+    }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,23 +1,28 @@
 import { Module } from "@nestjs/common";
-import { Field, Int } from "@nestjs/graphql";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserRepository } from "./user.repository";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import { JwtStrategy } from "./jwt.strategy";
+import * as config from "config";
 
-@Module({})
-export class UserModule {
-    @Field()
-    userId: string;
+const jwtConfig = config.get("jwt");
 
-    @Field()
-    name: string;
-
-    @Field()
-    nickname: string;
-
-    @Field(() => Int)
-    email: number;
-
-    @Field()
-    partner_id: string;
-
-    @Field({ nullable: true })
-    isAuth?: boolean;
-}
+@Module({
+    imports: [
+        PassportModule.register({ defaultStrategy: "jwt" }),
+        JwtModule.register({
+            secret: process.env.JWT_SECRET || jwtConfig.secret,
+            signOptions: {
+                expiresIn: jwtConfig.expiresIn,
+            },
+        }),
+        TypeOrmModule.forFeature([UserRepository]),
+    ],
+    controllers: [AuthController],
+    providers: [AuthService, JwtStrategy],
+    exports: [JwtStrategy, PassportModule],
+})
+export class AuthModule {}
