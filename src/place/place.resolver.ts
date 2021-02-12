c967416fc3644e7ff4aa4e592013bed7f31276ab
minkj1992 <minkj1992@gmail.com>
2021-02-12 16:04:48 +0900

    Rename: places

--- src/place/place.resolver.ts
+++ src/place/place.resolver.ts
@@ -9,6 +9,7 @@ export class PlaceResolver {
   constructor(private readonly searchService: SearchService) {}
 
   @Query(() => [Place], {
+    name: "places",
     description:
       "키워드로 위치 정보를 확인합니다. \n내부적으로 카카오 API를 요청합니다.",
   })
