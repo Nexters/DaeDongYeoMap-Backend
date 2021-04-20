47a7daa4860aa7ebe29e1decae7af243123f6d57
minkj1992 <minkj1992@gmail.com>
2021-04-20 19:15:28 +0900

    chore: sortSchema true

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,35 +1,35 @@
-import { Module } from "@nestjs/common";
-import * as path from "path";
+import { Module } from '@nestjs/common';
+import * as path from 'path';
 
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { GraphQLError, GraphQLFormattedError } from "graphql";
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLError, GraphQLFormattedError } from 'graphql';
 
-import { GraphQLModule } from "@nestjs/graphql";
-import { MongooseModule } from "@nestjs/mongoose";
-import { AppConfigModule } from "./config/config.module";
-import { AppConfigService } from "./config/config.service";
-import { PlaceModule } from "./place/place.module";
-import { SpotModule } from "./spot/spot.module";
-import { UserModule } from "./user/user.module";
-import { CourseModule } from "./course/course.module";
-import { StickerModule } from "./sticker/sticker.module";
-import { SharedModule } from "./shared/shared.module";
-import { AppResolver } from "./app.resolver";
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppConfigModule } from './config/config.module';
+import { AppConfigService } from './config/config.service';
+import { PlaceModule } from './place/place.module';
+import { SpotModule } from './spot/spot.module';
+import { UserModule } from './user/user.module';
+import { CourseModule } from './course/course.module';
+import { StickerModule } from './sticker/sticker.module';
+import { SharedModule } from './shared/shared.module';
+import { AppResolver } from './app.resolver';
 
 @Module({
   imports: [
     AppConfigModule,
     GraphQLModule.forRoot({
-      autoSchemaFile: path.join(process.cwd(), "src/schema.gql"),
+      autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
       debug: false,
       playground: true,
       introspection: true,
+      sortSchema: true,
       context: ({ req }) => ({ req }),
       formatError: (error: GraphQLError): GraphQLFormattedError => {
         const graphQLFormattedError: GraphQLFormattedError = {
-          message:
-            error.extensions?.exception?.response?.message || error.message,
+          message: error.extensions?.exception?.response?.message || error.message,
         };
 
         return graphQLFormattedError;
